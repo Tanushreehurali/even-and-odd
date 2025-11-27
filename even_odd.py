9862c4ad601a14b669f2baf4c854d1a7dfc12d07
Tanushreehurali <tanuhurali@gmail.com>
2025-11-27 15:08:10 +0530

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,27 @@
+import sys
+
+..
+if len(sys.argv) > 1:
+    script_name = sys.argv[0]
+    numbers = [int(x) for x in sys.argv[1:]]
+    print("User provided input numbers:")
+else:
+    script_name = sys.argv[0]
+    numbers = [10, 15, 22, 33, 40]  # default values
+    print("No input given — using default numbers:")
+
+
+even_count = 0
+odd_count = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+
+print("Script Name:", script_name)
+print("Numbers:", numbers)
+print("Even Count:", even_count)
+print("Odd Count:", odd_count)
